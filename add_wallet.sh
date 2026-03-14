b6ad4a73704ce63697a309e59ee6b9869f8d13fe
0xzrf <krishh16@yahoo.com>
2026-03-15 00:25:33 +0530

    removed un-necessary scripts

--- add_wallet.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wallets=("$@")
-wallets_json=$(printf '"%s",' "${wallets[@]}")
-wallets_json="[${wallets_json%,}]"
-
-curl -X POST -H "Content-Type: application/json" -d "{\"wallets\":$wallets_json}" http://localhost:3000/add_wallet
